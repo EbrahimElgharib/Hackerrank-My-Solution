4841518142e28703b6b8868886dc7d8bb1506300
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-07-27 15:37:34 +0300

    add ssss

---
+++ 3 Months Preparation Kit/Week3/Permuting Two Arrays.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoArrays' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY A
+#  3. INTEGER_ARRAY B
+#
+
+def twoArrays(k, n, A, B):
+    # Write your code here
+    A.sort()
+    B.sort(reverse=True)
+    
+    for i in range(n):
+        if A[i] + B[i] < k: return 'NO'
+    
+    return 'YES'
+
+if __name__ == '__main__':
+    
+    q = int(input().strip())
+    
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        A = list(map(int, input().rstrip().split()))
+
+        B = list(map(int, input().rstrip().split()))
+
+        result = twoArrays(k,n, A, B)
+
+        print(result + '\n')
